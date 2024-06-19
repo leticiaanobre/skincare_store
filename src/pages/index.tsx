43d08a1160b24101221d1920c7409ac0153188d0
Leticia Nobre <leticiavidal2910@gmail.com>
2024-06-19 18:20:26 -0300

    refat: recreated Home page in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,93 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import React from 'react';
+import { Box, Flex, Text, Spacer, Container, Heading, Button, Image } from '@chakra-ui/react';
+import img from '../../assets/skin-face.jpg';
+import littleBottle from '../../assets/little-bottle.jpg'
 
-const inter = Inter({ subsets: ["latin"] });
+const Home: React.FC = () => {
 
-export default function Home() {
+    const buttonStyle = {
+        width:"140px",
+        fontSize:"sm",
+        variant:"outline",
+        color:"white",
+        borderColor:"white",
+        mx:"2",
+        borderRadius:"16",
+        fontWeight:"300",
+        _hover: {
+            bg:"none",
+            borderWidth:"2px",
+        }
+    }
+    const buttonHeader = {
+        color:"white",
+        variant:"outline",
+        border:"none",
+        fontWeight:"300",
+        _hover: {
+            bg:"none",
+            color:"gray.700"
+        }
+    }
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box 
+        bgImage={`url(${img.src})`}
+        bgSize="cover"
+        bgPosition="center"
+        bgRepeat="no-repeat"
+        minHeight="100vh"
+        color="white"
+        position="relative"
+        bgColor="gray.600"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Flex as="header" py="2" px="8" alignItems="center">
+        <Text fontSize="xl" fontWeight="300" color="white">SKIN</Text>
+        <Spacer />
+        <Flex py="1" px="8" bg="rgba(150, 150, 150, 0.3)" borderRadius="12" backdropFilter="blur(10px)">
+          <Button {...buttonHeader}>Shop All</Button>
+          <Button {...buttonHeader}>About Us</Button>
+          <Button {...buttonHeader}>New</Button>
+        </Flex>
+      </Flex>
+      <Flex position="absolute" py="8" px="10" minWidth="100%" mt="9%">
+        <Flex justifyContent="space-between" w="100%" alignItems="center" >
+            <Flex direction="column" gap="4">
+                <Flex alignContent="start" justifyContent="start" direction="column">
+                    <Heading fontSize="7xl" color="white" mb="4" fontWeight="300" letterSpacing="0.35rem" >Find your</Heading>
+                    <Heading fontSize="7xl" color="white" mb="4" fontWeight="300" letterSpacing="0.35rem" width="800px">perfect Foundation!</Heading>
+                </Flex>
+                <Flex mb="4">
+                    <Button {...buttonStyle}>Face</Button>
+                    <Button {...buttonStyle}>Body</Button>
+                    <Button {...buttonStyle}>Hair</Button>
+                </Flex>
+                <Flex width="32rem">
+                    <Text color="white" fontSize="sm" bg="rgba(150, 150, 150, 0.3)" p="3" borderRadius="18" backdropFilter="blur(10px)">
+                    A highly concentrated regenerating cream with a powerful antioxidant effect. It is rich in vitamins A, C, E.
+                    </Text>
+                </Flex>
+            </Flex>
+            <Flex direction="column" minWidth="300px" maxWidth="300px" bg="rgba(150, 150, 150, 0.3)" backdropFilter="blur(10px)" borderRadius="18" py="5" px="8" alignItems="center" gap="3" height="100%">
+                <Flex alignContent="center">
+                    <Image 
+                        borderRadius="full"
+                        boxSize="200px"
+                        objectFit="cover"
+                        src={littleBottle.src}/>
+                </Flex>
+                <Flex direction="column" alignItems="center">
+                    <Text fontSize="12">Moisturizing</Text>
+                    <Text fontSize="24" fontWeight="600">$150</Text>
+                    <Text fontSize="16" align="center">Skincare Vital C hydrating Repair Creme</Text>
+                </Flex>
+                <Flex mb="4">
+                    <Button borderRadius="18" px="8" fontWeight="500">Shop now</Button>
+                </Flex>
+            </Flex>
+        </Flex>
+      </Flex>
+    </Box>
   );
 }
+
+export default Home;
