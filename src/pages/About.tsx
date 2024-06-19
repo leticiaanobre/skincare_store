54f6a05900e15dca797fd11cecfb616268e30e0d
Leticia Nobre <leticiavidal2910@gmail.com>
2024-06-19 18:27:32 -0300

    feat:created about us page

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+import {Text, Container} from '@chakra-ui/react'
+
+export default function About() {
+    return(
+        <>
+            <Container id="about">
+                <Text>About Us</Text>
+            </Container>
+        </>
+    )
+}
