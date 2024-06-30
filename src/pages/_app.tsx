c665cab6578310525c5d4aaeb9c624812918e9f3
Leticia Nobre <leticiavidal2910@gmail.com>
2024-06-30 12:08:56 -0300

    refat: refact app structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,28 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider} from '@chakra-ui/react'
+import { ChakraProvider, Box } from '@chakra-ui/react';
+import Navbar from "./navbar";
+import { useState } from 'react';
+import img from '../../assets/skin-face.jpg';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [backgroundImage, setBackgroundImage] = useState(img.src);
+
   return (
-    <>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </>
+    <ChakraProvider>
+      <Box 
+        bgImage={`url(${backgroundImage})`}
+        bgSize="cover"
+        bgPosition="center"
+        bgRepeat="no-repeat"
+        minHeight="100vh"
+        color="white"
+        position="relative"
+        bgColor="gray.600"
+      >
+        <Navbar />
+        <Component {...pageProps} setBackgroundImage={setBackgroundImage} />
+      </Box>
+    </ChakraProvider>
   );
 }
