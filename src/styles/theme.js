dfe2dfcdb8d92e763cce1a48ab3cfc76f572d3d9
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-22 19:41:05 -0300

    💄 style: added theme.js file

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    primary: '#67645E',
+    secondary: 'rgba(204, 204, 204, 0.6)',
+    tertiary: '#E5E5E5',
+  },
+});
+
+export default theme;
