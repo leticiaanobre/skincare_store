c299e51b257cd0934fa8931c29ecbd9a7bd66a98
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-22 16:17:34 -0300

    💄 style: changed navbarText color and added new variant

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -22,18 +22,18 @@ export default function Navbar() {
   return (
     <Flex p="2" w='full' height="60px" position='relative'>
       <Flex as="header" px="8" alignItems="center" w='full' bg="var(--color-secondary)" borderRadius='12'>
-        <Text fontSize="xl" fontWeight="300" color="var(--color-primary)">SKIN</Text>
+        <Text fontSize="xl" fontWeight="300" color="var(--color-navbarText)">SKIN</Text>
         <Spacer />
         <Flex py="1" px="8">
-          <Button {...buttonHeader} color="var(--color-primary)" onClick={() => scrollToSection('home')}>
+          <Button {...buttonHeader} color="var(--color-navbarText)" onClick={() => scrollToSection('home')}>
             Home
           </Button>
-          <Button {...buttonHeader} color="var(--color-primary)">Shop All</Button>
-          <Button {...buttonHeader} color="var(--color-primary)" onClick={() => scrollToSection('about')}>
+          <Button {...buttonHeader} color="var(--color-navbarText)" onClick={() => scrollToSection('about')}>
             About Us
           </Button>
+          <Button {...buttonHeader} color="var(--color-navbarText)">Shop All</Button>
           <Menu isLazy>
-            <MenuButton {...buttonHeader} px='16px' color="var(--color-primary)">More</MenuButton>
+            <MenuButton {...buttonHeader} px='16px' color="var(--color-navbarText)">More</MenuButton>
             <MenuList bg="rgba(150, 150, 150, 0.3)" borderRadius="12" border='none' backdropFilter="blur(10px)">
               <MenuItem bg='none'>New Window</MenuItem>
               <MenuItem bg='none'>Open Closed Tab</MenuItem>
