391f56371277b5ba9cfefb4bc875670f36f008c3
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-22 16:09:18 -0300

    ♻️ refat: readjusted About page to be a scroll section and not a static one

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,19 +1,10 @@
-import { useEffect } from 'react';
 import { Box, Flex, Text } from '@chakra-ui/react';
 import Image from 'next/image';
-import img from '../../assets/model2.jpg';
+import img from '../../../assets/model2.jpg';
 
-interface AboutProps {
-  setBackgroundStyle: (style: { type: string; value: string }) => void;
-}
-
-export default function About({ setBackgroundStyle }: AboutProps) {
-  useEffect(() => {
-    setBackgroundStyle({ type: 'color', value: 'var(--color-background)' });
-  }, [setBackgroundStyle]);
-
+export default function About() {
   return (
-    <Box height="calc(100vh - 60px)" overflowY="auto" display="flex" alignItems="center" justifyContent="center"  pt='0'>
+    <Box height="100vh" id="about" overflowY="auto" display="flex" alignItems="center" justifyContent="center" pt='0' bgColor='background'>
       <Flex direction="row" alignItems="center" justifyContent='space-around' p='3' height="100%" w='full'>
         <Box
           position="relative"
@@ -33,11 +24,15 @@ export default function About({ setBackgroundStyle }: AboutProps) {
           />
         </Box>
         <Flex direction='column' p='6' w='40%' align='center' gap='3'>     
-            <Text as="h1" fontSize="4xl" fontWeight="semibold" color="var(--color-primary)">
+          <Text as="h1" fontSize="4xl" fontWeight="semibold" color="var(--color-primary)">
             ABOUT
-            </Text>
-            <Text color="var(--color-primary)">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus voluptates similique, voluptatibus error fuga vero excepturi quasi exercitationem porro obcaecati, fugiat corporis.</Text>
-            <Text color="var(--color-primary)">Lorem ipsum dolor sit amet consectetur adipisicing elit. In, eius quidem magni soluta recusandae eaque, pariatur, provident esse tempora blanditiis dolor beatae fugiat quisquam labore quaerat repudiandae atque? Modi aut ea mollitia consequatur dolore repudiandae maiores architecto laboriosam? Accusamus minus soluta, placeat, aut tenetur laudantium eligendi iure magnam ex eum ratione sunt doloribus temporibus earum?</Text>
+          </Text>
+          <Text color="var(--color-primary)">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus voluptates similique, voluptatibus error fuga vero excepturi quasi exercitationem porro obcaecati, fugiat corporis.
+          </Text>
+          <Text color="var(--color-primary)">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In, eius quidem magni soluta recusandae eaque, pariatur, provident esse tempora blanditiis dolor beatae fugiat quisquam labore quaerat repudiandae atque? Modi aut ea mollitia consequatur dolore repudiandae maiores architecto laboriosam? Accusamus minus soluta, placeat, aut tenetur laudantium eligendi iure magnam ex eum ratione sunt doloribus temporibus earum?
+          </Text>
         </Flex>
       </Flex>
     </Box>
