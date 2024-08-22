737ed2ee90d42786f9fdc76186571e32387e5edd
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-22 16:08:41 -0300

    ♻️ refat: readjusted app component to have only box children

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider, Box } from '@chakra-ui/react';
+import { ChakraProvider, Box } from "@chakra-ui/react";
 import Navbar from "../components/Navbar/navbar";
-import { useState } from 'react';
-import img from '../../assets/skin-face.jpg';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [backgroundStyle, setBackgroundStyle] = useState({
-    type: 'image',
-    value: img.src,
-  });
-
   return (
     <ChakraProvider>
-      <Box 
-        bgImage={backgroundStyle.type === 'image' ? `url(${backgroundStyle.value})` : 'none'}
-        bgColor={backgroundStyle.type === 'color' ? backgroundStyle.value : 'gray.600'}
-        bgSize="cover"
-        bgPosition="center"
-        bgRepeat="no-repeat"
-        minHeight="100vh"
+      <Box
+        bg="gray.600"
         color="white"
-        position="relative"
+        minHeight="100vh"
+        display="flex"
+        flexDirection="column"
       >
-        <Navbar setBackgroundStyle={setBackgroundStyle} />
-        <Box display="flex" flexDirection="column" height="calc(100vh - 60px)">
-          <Component {...pageProps} setBackgroundStyle={setBackgroundStyle} />
+        {/* <Navbar /> */}
+        <Box as="main" flex="1">
+          <Component {...pageProps} />
         </Box>
       </Box>
     </ChakraProvider>
